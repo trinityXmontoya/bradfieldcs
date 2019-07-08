1530a72deba71bbb3f4f34c6f246bf4cc2c94a2d
trinityXmontoya <trinityXmontoya@gmail.com>
2019-07-08 13:50:55 -0700

    lil cleanup

--- src/algos/methodical_problem_solving/prework/pangram.clj
+++ src/algos/methodical_problem_solving/prework/pangram.clj
@@ -11,7 +11,7 @@
 ;2. Devise a plan
 ;Write tests as a way to suss out possible edge-cases (capitalization, punctuation)
 ;Think about possible data structures / needed variables / approaches (list of all 26 chars, sets, regex)
-;The idea of a set is attractive because they guarantee we care about membership not frequency of each individual
+;The idea of a set is attractive because we care about membership not frequency of each individual
 ;character and lookup is O(1).
 ;The idea of a regex is attractive because it doesn't require me writing out or copy/pasting all 26 letters of the alphabet
 ;but I've heard they can be computationally expensive to use and if the intention is general purpose problem-solving perhaps
@@ -22,13 +22,13 @@
 ;on all inputs or one that usually runs quickly but can take years of CPU time (or more) on some inputs, the decision should be easy."
 ;
 ;3. Carry out the plan
-(def chars-of-the-alphabet-1
-  #{\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z})
+(def alphabet-as-set
+  (set "abcdefghijklmnopqrstuvwxyz"))
 
 (defn pangram-1?
-  [s]
-  (let [s-as-set (set (str/lower-case s))]
-    (every? s-as-set chars-of-the-alphabet-1)))
+  [string]
+  (let [string-as-set (set (str/lower-case string))]
+    (every? string-as-set alphabet-as-set)))
 
 ;4. Look back
 ;Passes tests, but can it be solved w/o creating a set? Even if set lookup is efficient, this requires iterating
@@ -47,39 +47,68 @@
 ;tail-call optimization but it does have `loop`/`recur` ["which does constant-space recursive looping"](https://clojure.org/about/functional_programming#_recursive_looping)
 
 ;3. Carry out the plan
-(def chars-of-the-alphabet-2
+(def alphabet-as-vec
   ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"])
 
 (defn pangram-2?
-  [s]
-  (let [lower-cased-s (str/lower-case s)]
-    (loop [current-char (first chars-of-the-alphabet-2)
-           remaining-chars (rest chars-of-the-alphabet-2)]
+  [string]
+  (let [lower-cased-string (str/lower-case string)]
+    (loop [current-char (first alphabet-as-vec)
+           remaining-chars (rest alphabet-as-vec)]
       (cond
         (nil? current-char) true
-        (str/includes? lower-cased-s current-char) (recur (first remaining-chars) (rest remaining-chars))
+        (str/includes? lower-cased-string current-char) (recur (first remaining-chars) (rest remaining-chars))
         :else false))))
+
+; similar
+; (defn pangram-2?
+;  [string]
+;  (let [lower-cased-string (str/lower-case string)]
+;    (= (count alphabet-as-vec)
+;       (count
+;         (for [char alphabet-as-vec
+;               :while (str/includes? lower-cased-string char)]
+;           char)))))
 
 ;4. Look back
 ;Passes tests, but I don't love that I'm:
 ;- checking if the current character is nil each time
 ;- checking the entire input string each time (`str/includes?` uses Java's `String.contains` under the hood which uses
-;`String.indexOf` which has O(N) complexity but I'm running it between up to 26 times)
+;`String.indexOf` which has O(N) complexity but I'm running it up to 26 times).
 
 
 ;----------------
 ;APPROACH 3
 ;----------------
 ;1. Understand the problem:
-;Can I avoid checking the entire sentence each time?
+;Write a function that determines if a string is a pangram w/o checking the entire sentence each time.
 ;
 ;2. Devise a plan
 ;If I don't want to check the entire sentence each time I could keep a separate record of which characters I've found
-;so far. If I build *up* a set, I'd have to be checking whether each character was indeed part of the original 26 which
-;negates benefits of doing it this way. I could build *down* a set but Clojure's data structures are immutable and I'm not
-;sure what the implications of creating a new set each time I attempt to remove an element would be.
+;so far. If I build *up* a set, I'd have to check before each insert whether the character is part of the alphabet.
+;If I build *down* a set, I would already have the 26 characters. Either way Clojure's data structures are immutable and
+;I'm not sure what the performance implications of creating a new set each time I add / remove an element would be.
+; Some pseduocode:
+;for char in string
+;  if char in alphabet
+;    if already in final-set
+;      next
+;    elsif count final-set == 25
+;      true
+;      exit
+;    else
+;      next
+;    end
+;  else
+;    next
+;  end
+;end
+; false
 
 ;3. Carry out the plan
 (defn pangram-3?
-  [s])
+  [string]
+  (let [lower-cased-string (str/lower-case string)]))
+
 ;4. Look back
+;
