bbf95c1afbdc7fa920b5397253c28c43debf4a96
trinityXmontoya <trinityXmontoya@gmail.com>
2019-07-17 18:54:59 -0700

    refactor tests

--- test/algos/methodical_problem_solving/prework/pangram_test.clj
+++ test/algos/methodical_problem_solving/prework/pangram_test.clj
@@ -2,26 +2,31 @@
   (:require [clojure.test :refer :all]
             [algos.methodical_problem_solving.prework.pangram :refer :all]))
 
+(def pangrams
+  ["the quick brown fox jumps over the lazy dog"
+   "My girl wove six dozen plaid jackets before she quit!"
+   "J.Q. Schwartz flung V.D. Pike my box"])
+
+(def not-pangrams
+  ["Sadly, I am not a pangram :("])
+
 (deftest pangram-1-test
-  (testing "is the given string a pangram according to pangram-2 fn?"
-    (are [res str] (= (pangram-1? str) res)
-                   true "the quick brown fox jumps over the lazy dog"
-                   true "My girl wove six dozen plaid jackets before she quit!"
-                   true "J.Q. Schwartz flung V.D. Pike my box"
-                   false "Sadly, I am not a pangram :(")))
+  (testing "is the given string a pangram according to pangram-1 fn?"
+    (doseq [pangram pangrams]
+      (is (true? (pangram-1? pangram)) "for known valid pangrams"))
+    (doseq [pangram not-pangrams]
+      (is (false? (pangram-1? pangram)) "for known invalid pangrams"))))
 
 (deftest pangram-2-test
   (testing "is the given string a pangram according to pangram-2 fn?"
-    (are [res str] (= (pangram-2? str) res)
-                   true "the quick brown fox jumps over the lazy dog"
-                   true "My girl wove six dozen plaid jackets before she quit!"
-                   true "J.Q. Schwartz flung V.D. Pike my box"
-                   false "Sadly, I am not a pangram :(")))
+    (doseq [pangram pangrams]
+      (is (true? (pangram-2? pangram)) "for known valid pangrams"))
+    (doseq [pangram not-pangrams]
+      (is (false? (pangram-2? pangram)) "for known invalid pangrams"))))
 
 (deftest pangram-3-test
   (testing "is the given string a pangram according to pangram-3 fn?"
-    (are [res str] (= (pangram-3? str) res)
-                   true "the quick brown fox jumps over the lazy dog"
-                   true "My girl wove six dozen plaid jackets before she quit!"
-                   true "J.Q. Schwartz flung V.D. Pike my box"
-                   false "Sadly, I am not a pangram :(")))
+    (doseq [pangram pangrams]
+      (is (true? (pangram-3? pangram)) "for known valid pangrams"))
+    (doseq [pangram not-pangrams]
+      (is (false? (pangram-3? pangram)) "for known invalid pangrams"))))
