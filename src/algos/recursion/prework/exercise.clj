a5cd43f3ea23ed46977a68b86abb6d343b2c399a
trinityXmontoya <trinityXmontoya@gmail.com>
2019-07-21 12:50:40 -0700

    different base case for sum of #s to prevent last call

--- src/algos/recursion/prework/exercise.clj
+++ src/algos/recursion/prework/exercise.clj
@@ -1,13 +1,31 @@
 (ns algos.recursion.prework.exercise)
 
+;----------
+;INTRO
+;----------
 (defn sum-of-nums
   [nums]
   (reduce + nums))
 
-(defn sum-of-nums-recursive
+;base case: empty list
+(defn sum-of-nums-recursive-1
   [nums]
   (loop [remaining-nums (rest nums)
          final-sum (first nums)]
     (if (empty? remaining-nums)
       final-sum
       (recur (rest remaining-nums) (+ final-sum (first remaining-nums))))))
+
+;base case: list of 1 element, saves last call
+(defn -add-first-el
+  [sum nums]
+  (+ sum (first nums)))
+
+(defn sum-of-nums-recursive-2
+  [nums]
+  (loop [remaining-nums (rest nums)
+         final-sum (first nums)]
+    (if (= 1 (count remaining-nums))
+      (-add-first-el final-sum remaining-nums)
+      (recur (rest remaining-nums)
+             (-add-first-el final-sum remaining-nums)))))
--- test/algos/recursion/prework/exercise_test.clj
+++ test/algos/recursion/prework/exercise_test.clj
@@ -5,5 +5,8 @@
 (deftest sum-of-nums-test
   (is (= (sum-of-nums [1 3 5 7 9]) 25)))
 
-(deftest sum-of-nums-recursive-test
-  (is (= (sum-of-nums-recursive [1 3 5 7 9]) 25)))
+(deftest sum-of-nums-recursive-1-test
+  (is (= (sum-of-nums-recursive-1 [1 3 5 7 9]) 25)))
+
+(deftest sum-of-nums-recursive-2-test
+  (is (= (sum-of-nums-recursive-2 [1 3 5 7 9]) 25)))
