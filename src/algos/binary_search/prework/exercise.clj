7b9a1c3709824d1466848963497f97bc730cada9
trinityXmontoya <trinityXmontoya@gmail.com>
2019-07-25 00:16:43 -0700

    search w indices

--- src/algos/binary_search/prework/exercise.clj
+++ src/algos/binary_search/prework/exercise.clj
@@ -1,6 +1,7 @@
 (ns algos.binary_search.prework.exercise)
 
-(defn binary-search
+;An implementation of recursive binary search:
+(defn binary-search-recursive
   [coll item]
   (loop [coll coll]
     (cond
@@ -21,3 +22,21 @@
 ; create a 'new' vector we copy *references* to the original vector rather than the values themselves which saves having
 ; to copy the entire vector.
 ; (src: https://www.infoq.com/articles/in-depth-look-clojure-collections/, CMD+F 'Clojure's persistent collections' + https://hackernoon.com/how-immutable-data-structures-e-g-immutable-js-are-optimized-using-structural-sharing-e4424a866d56)
+;
+; After further thought there's ~no~ reason I have to manipulate or pass around the vector at all, I can just use the indices.
+; I also remove the check for whether it's within the given range bc I don't know if that goes against the implementation of binary search(?)
+; Doesn't seem like it should and it prevents you from doing unnecessary math but jic...
+(defn binary-search
+  [coll item]
+  (if (empty? coll)
+    false
+    (loop [lower-bound 0
+           upper-bound (dec (count coll))]
+      (if
+        (< (- upper-bound lower-bound) 1) (= (coll lower-bound) item)
+        (let [midpoint (quot (+ lower-bound upper-bound) 2)
+              midpoint-el (coll midpoint)]
+          (cond
+            (= item midpoint-el) true
+            (< item midpoint-el) (recur lower-bound (dec midpoint))
+            (> item midpoint-el) (recur (inc midpoint) upper-bound)))))))
--- test/algos/binary_search/prework/exercise_test.clj
+++ test/algos/binary_search/prework/exercise_test.clj
@@ -2,8 +2,8 @@
   (:require [clojure.test :refer :all]
             [algos.binary_search.prework.exercise :refer :all]))
 
-(deftest  binary-search-test
-    (are [res coll item] (= (binary-search coll item) res)
+(deftest  binary-search-recursive-test
+    (are [res coll item] (= (binary-search-recursive coll item) res)
                        false [0, 1, 2, 8, 13, 17, 19, 32, 42] 3   ; example from reading
                        false [] 3                                 ; 0 el array
                        false [4] 3                                ; 1 el array
@@ -14,3 +14,16 @@
                        true  [0, 1, 2, 8, 13, 17, 19, 32, 42] 42  ; el is last val
                        true  [0, 1, 2, 2, 2, 8, 13, 17, 19, 32] 2 ; repeated els
      ))
+
+(deftest  binary-search-test
+  (are [res coll item] (= (binary-search coll item) res)
+                       false [0, 1, 2, 8, 13, 17, 19, 32, 42] 3   ; example from reading
+                       false [] 3                                 ; 0 el array
+                       false [4] 3                                ; 1 el array
+                       false [0, 1, 2, 2, 2, 8, 13, 17, 19, 32] 3 ; repeated els
+
+                       true  [0, 1, 2, 8, 13, 17, 19, 32, 42] 13  ; example from reading
+                       true  [4] 4                                ; 1 el array
+                       true  [0, 1, 2, 8, 13, 17, 19, 32, 42] 42  ; el is last val
+                       true  [0, 1, 2, 2, 2, 8, 13, 17, 19, 32] 2 ; repeated els
+                       ))
