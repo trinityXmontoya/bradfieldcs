a6ab37f9f652861511776d25341ea943c920699a
trinityXmontoya <trinityXmontoya@gmail.com>
2019-07-24 20:31:47 -0700

    binary search notees

--- src/algos/binary_search/prework/exercise.clj
+++ src/algos/binary_search/prework/exercise.clj
@@ -12,3 +12,10 @@
           (= el midpoint-el) true
           (< el midpoint-el) (recur (subvec coll 0 midpoint))
           (> el midpoint-el) (recur (subvec coll (inc midpoint))))))))
+
+;4. Looking back
+; unlike Python's slice, subvec is O(1)
+; There is a memory cost to this but because Clojure uses structural sharing and implements vectors as trees, when we
+; create a 'new' vector we copy *references* to the original vector rather than the values themselves which saves having
+; to copy the entire vector.
+; (src: https://www.infoq.com/articles/in-depth-look-clojure-collections/, CMD+F 'Clojure's persistent collections' + https://hackernoon.com/how-immutable-data-structures-e-g-immutable-js-are-optimized-using-structural-sharing-e4424a866d56)
