5bcc2a0bf841320daa44523e567dbf02f84b0fe4
trinityXmontoya <trinityXmontoya@gmail.com>
2019-07-28 23:13:17 -0700

    multimethod approach

--- src/algos/divide_and_conquer/prework/inversion.clj
+++ src/algos/divide_and_conquer/prework/inversion.clj
@@ -3,7 +3,24 @@
 ;Count the # of inversions in an array
 
 (defn num-of-inversions
-  [coll]
-  (if (< 2 (count coll))
-    0
-    ()))
+  ([coll]
+   (if (< (count coll) 2)
+     0
+     (apply
+       +
+       (keep-indexed
+         (fn [outer-idx outer-el]
+           (let [sub-coll (subvec coll (inc outer-idx))]
+             (num-of-inversions sub-coll outer-el)))
+         coll))))
+  ([coll el]
+    (apply
+      +
+      (keep
+        (fn [inner-el]
+          (when (> el inner-el) 1))
+        coll))))
+
+
+
+
--- test/algos/divide_and_conquer/prework/inversion_test.clj
+++ test/algos/divide_and_conquer/prework/inversion_test.clj
@@ -4,9 +4,9 @@
 
 
 (deftest num-of-inversions-test
-  (testing "are the given strings anagrams?"
     (are [res coll] (= (num-of-inversions coll) res)
-                    3 [2,  4, 1, 3, 5]
                     5 [1, 20, 6, 4, 5]
+                    3 [2,  4, 1, 3, 5]
+                    1 [2 1]
                     0 [1]
-                    0 [])))
+                    0 []))
