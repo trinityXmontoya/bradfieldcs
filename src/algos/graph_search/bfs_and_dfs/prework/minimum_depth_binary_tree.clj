a49540de8bdf198c2056b9ac2fc2869d739c1589
trinityXmontoya <trinityXmontoya@gmail.com>
2019-08-01 16:54:42 -0700

    more updates

--- src/algos/graph_search/bfs_and_dfs/prework/minimum_depth_binary_tree.clj
+++ src/algos/graph_search/bfs_and_dfs/prework/minimum_depth_binary_tree.clj
@@ -20,57 +20,61 @@
 ;return its minimum depth = 2.
 
 ;2. Devise a plan
-; We know that in a binary tree represented as an array, the index of the parent node for a given child
-; is n//2.
-; We can find the first leaf node by finding the index of the first nil.
-; Then, we continue to //2 until we reach 1 (the root node)
+; We know that in a binary tree represented as an array, the index of the parent node (P) for a given child index (N)
+; is P = (N + (root_index - 1)) // 2.
+; We can find the first leaf node by finding the parent index of the first nil.
+; Then, we continue to find the parent index until we reach 0 (the root node)
 ; This is worst case O(N) performance.
 
 ;3. Carry out plan
+(declare leaf-node?)
+
+(defn parent-idx
+  [idx]
+  (let [root-idx 0]
+    (quot (+ idx (dec root-idx)) 2)))
+
 (defn min-depth-w-math
   [bin-tree]
   (let [leaf-node-idx (first
                         (keep-indexed
                           (fn [idx el]
                             (when (nil? el) idx)) bin-tree))
-        steps-to-parent (take-while #(> % 1) (iterate #(quot % 2) leaf-node-idx))]
-    (count steps-to-parent)))
+        steps-to-root (take-while #(> % 0) (iterate parent-idx leaf-node-idx))]
+    (count steps-to-root)))
 
 ;4. Look back
-; This only worked if I added a 0 to the front of each array :(
-; This also doesn't work for incomplete binary trees
+; This doesn't work for incomplete binary trees
 ; ex.
 ; 1
 ;	 \
 ;	  2
 ;	 /
 ;	3
-; because that'd be represented like this
-; [0, 1, nil, 2, nil, nil, 3, nil]
-; and then the first nil is a child of the root node but the root node wouldn't be considered a leaf node.
-; Continuing to represent the non-existent children of a non-existent node causes other problems (when
-; trying to determine the first nil non-root-node-child index i'd have to ignore any idx < 2 but also
-; whose parent is nil). Sample code below accomplishes that but also adds the complexity of looking up
-; 3 other nodes
-;
-;(defn leaf-node?
-;  [idx coll]
-;  (let [node (get coll idx)
-;        parent (get coll (quot idx 2))
-;        left-child (get coll (* 2 idx))
-;        right-child (get coll (inc (* 2 idx)))]
-;    (and
-;      (not (nil? node))
-;      (not (nil? parent))
-;      (nil? left-child)
-;      (nil? right-child))))
-; And again, this only works if the binary tree is represented in a specific format.
+; because that'd be represented as
+; [1, nil, 2, nil, nil, 3, nil, nil, nil, nil, nil]
+; the first nil in this case is the child of root node which isn't a leaf node
+; the second nil is the child of a nonexistent parent
+; I could identify the first leaf node by confirming the parent is *not* nil and doesn't have other children
+; but then for every node i'm checking all its connected ones
+(defn leaf-node?
+  [idx coll]
+  (let [node (get coll idx)
+        parent (get coll (parent-idx idx))
+        left-child (get coll (+ 1 (* 2 idx)))
+        right-child (get coll (+ 2 (* 2 idx)))]
+    (and
+      (nil? node)
+      (not (nil? parent))
+      (nil? left-child)
+      (nil? right-child))))
 ; Back to the drawing board to use my newfound graph knowledge as this lesson originally intended!
 
 
 ;2. Devise a plan
 ; Build and use a graph to find the first leaf node
-
+; since we want the first leaf node
+;
 ;3. Carry out plan
 (defn node
   [val]
@@ -80,14 +84,38 @@
 
 (defn build-tree
   [coll]
-
-
-  )
+  {
+   :val 3
+   :left {
+          :val 9
+          :left nil
+          :right nil
+          }
+   :right {
+           :val 20
+           :left {
+                  :val 15
+                  }
+           :right {
+                   :val 7
+                   }
+           }
+   })
 
 (defn min-depth
   [coll]
   (let [bin-tree (build-tree coll)]
+    (loop [tree bin-tree]
+      (let [root (tree :val)
+            left (tree :left)
+            right (tree :right)]
 
+
+
+        )
+
+
+      )
     ))
 
 ;4. Look back
