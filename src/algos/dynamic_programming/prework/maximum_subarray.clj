8201a7a0ce35240f4f5348849d09840d47eec09b
trinityXmontoya <trinityXmontoya@gmail.com>
2019-08-08 15:53:11 -0700

    subarray skeleton

---
+++ src/algos/dynamic_programming/prework/maximum_subarray.clj
@@ -0,0 +1,19 @@
+(ns algos.dynamic_programming.prework.maximum_subarray)
+
+;1. Understand the problem
+;src: https://leetcode.com/problems/maximum-subarray/description/
+;Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+;
+;Example:
+;
+;Input: [-2,1,-3,4,-1,2,1,-5,4],
+;Output: 6
+;Explanation: [4,-1,2,1] has the largest sum = 6.
+
+;2. Devise a plan
+
+;3. Carry out plan
+(defn max-subarray
+  [nums])
+
+;4. Look back
---
+++ test/algos/dynamic_programming/prework/maximum_subarray_test.clj
@@ -0,0 +1,3 @@
+(ns algos.dynamic_programming.prework.maximum_subarray_test
+  (:require [clojure.test :refer :all]
+            [algos.dynamic_programming.prework.maximum_subarray :refer :all]))
